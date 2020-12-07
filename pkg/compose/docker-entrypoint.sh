f657e96293b5d3956a39a91aeb401dffbdd470d2
Vladimir Bogretsov <v.bogretsov@itransition.com>
2020-12-07 14:00:06 +0300

    Added runner hostname

--- pkg/compose/docker-entrypoint.sh
+++ pkg/compose/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env sh
+set -e
 
 while [ ! -S /var/run/docker.sock ]; do
     echo "waiting for docker socket ..."
     sleep 1
 done
 
-exec docker-compose $@
+if [ -f /run/hostname ]; then
+    HOSTNAME=$(cat /run/hostname)
+    exec HOSTNAME=${HOSTNAME} docker-compose $@
+else
+    exec docker-compose $@
+fi
